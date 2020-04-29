64cfd83e7f5a76bb83aa29b91aed9cdda9098c09
fatalixan <fatalixan@gmail.com>
2020-04-29 15:49:29 +0500

    Устанока плагинова django-allauth, python-decouple. Вынесение приватных настроект в .env

--- medconsulting/settings.py
+++ medconsulting/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=1)-(*g*m^3(xvmmcxg)ict((#!#mbdc%wvgl_&maz!z8o&-eu'
+SECRET_KEY = config('DEBUG', cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'medconsulting.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 
