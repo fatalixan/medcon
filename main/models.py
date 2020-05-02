9cab4543ee4e8f3d972f2fe206e5c5a22d603a6f
fatalixan <fatalixan@gmail.com>
2020-05-02 15:35:25 +0500

    Обновление моделей - профиль

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -11,41 +13,24 @@ class Query(models.Model):
 class Consultation(models.Model):
 
     medical_report = models.TextField("Report")
-    date_of_consultation = models.DateTimeField
+    date_of_consultation = models.DateTimeField()
 
-class User(models.Model):
-
-    email = models.CharField(max_length=45)
-    password = models.CharField(max_length=20)
-    date_of_registration = models.DateTimeField
 
 class Profile(models.Model):
-
-    name = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
     tnumber = models.CharField(max_length=50)
-    bday = models.DateField
-    sex = models.BooleanField
+    sex = models.BooleanField()
     lang = models.CharField(max_length=3)
 
 class Medical_record(models.Model):
+    information = models.CharField(max_length=255)
     pass
 
 class Consultation_status(models.Model):
 
     name = models.CharField("Consultation status", max_length=50)
 
-class Auth_group(models.Model):
 
-    name = models.CharField(max_length=30)
-
-class Auth_permission(models.Model):
-    pass
-
-class Auth_permission_group(models.Model):
-    pass
-
-class Content(models.Model):
-    name = models.CharField(max_length=30)
 
 
